19ceee79de135fd02e16182546e41859d34980df
Hrithik333 <67138916+Hrithik333@users.noreply.github.com>
2021-10-16 21:59:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,59 @@
+import { Route, Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+
+// redux actions
+import { getCurrUser } from "./Redux/Reducers/User/user.action";
+
+// HOC
+import HomeLayoutHOC from "./HOC/Home.HOC";
+import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
+import CheckoutLayoutHOC from "./HOC/Checkout.HOC";
+
+// Pages
+import Home from "./Page/Home";
+import Overview from "./Page/Restaurant/Overview";
+import OrderOnline from "./Page/Restaurant/OrderOnline";
+import Reviews from "./Page/Restaurant/Reviews";
+import Menu from "./Page/Restaurant/Menu";
+import Photos from "./Page/Restaurant/Photos";
+import Checkout from "./Page/Checkout";
+import RedirectRestaurant from "./Page/Restaurant/Redirect";
+import GoogleAuth from "./Page/GoogleAuth";
+import { useEffect } from "react";
+
+// axios global settings
+if (localStorage.zomatoUser) {
+  const { token } = JSON.parse(localStorage.zomatoUser);
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.zomatoUser) {
+      dispatch(getCurrUser())
+    }
+  }, [dispatch])
+  return (
+    <>
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
+      </Route>
+      <Route path="/restaurant/:id" exact component={RedirectRestaurant} />
+
+      <HomeLayoutHOC path="/:type" exact component={Home} />
+
+      <HomeLayoutHOC path="/google/:token" exact component={GoogleAuth} />
+      <RestaurantLayoutHOC path="/restaurant/:id/overview" exact component={Overview} />
+      <RestaurantLayoutHOC path="/restaurant/:id/order-online" exact component={OrderOnline} />
+      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Menu} />
+      <RestaurantLayoutHOC path="/restaurant/:id/photos" exact component={Photos} />
+      <RestaurantLayoutHOC path="/restaurant/:id/reviews" exact component={Reviews} />
+      <CheckoutLayoutHOC path="/checkout/orders" exact component={Checkout} />
+    </>
+  );
+}
+
+export default App;
